2ebad731d15166108ea8557814ac1d7525c3e461
Tom Öst <tom.ost@yahoo.com>
2020-01-29 23:17:57 +0700

    Created hamburger menu content and styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 const navbar = {
-    "logo": {
+    logo: {
         text1: "Portfolio",
         text2: "iamtt.tech",
     },
@@ -11,33 +11,74 @@ const navbar = {
         link1: {
             name: "Github",
             imgSrc: "assets/images/github_logo.png",
-            link: "https://www.github.com"
+            link: "https://github.com/tomost2019/"
         },
         link2: {
+            name: "Linkedin",
+            imgSrc: "assets/images/linkedin_logo.png",
+            link: "https://www.linkedin.com/in/tom-amnuay/"
+        },
+        link3: {
             name: "Line",
             imgSrc: "assets/images/line_logo.png",
             link: ""
-        },
-        link3: {
-            name: "Linkedin",
-            imgSrc: "assets/images/linkedin_logo.png",
-            link: "https://www.linkedin.com"
-        },
+        }
       },
       showLinks: () => {
         let social = Object.values(navbar.socialLinks);
         for (let value of social) {
           $(".social-icons").append(`
-                  <div class="item-social">
-                      <a href="${value.link}" target="_rel">
-                      <img src="${value.imgSrc}" alt="${value.name}">
-                      </a>
-                      <div class="item-social-text">
-                      <div class="underscore"></div>
-                      <span class="open">Open</span>
-                  </div>
-                `);
+            <div class="item-social">
+                <a href="${value.link}" target="_rel">
+                <img src="${value.imgSrc}" alt="${value.name}">
+                </a>
+            <div class="item-social-text">
+                <div class="underscore"></div>
+                <span class="open">Open</span>
+            </div>
+        `);
         }
+      },
+      hamburgerMenu: {
+        link1: {
+            text: "Projects",
+            href: ""
+        },
+        link2: {
+            text: "About",
+            href: ""
+        },
+        link3: {
+            text: "Skills",
+            href: ""
+        },
+        link4: {
+            text: "Contact",
+            href: ""
+        },
+      },
+      showMenu: () => {
+        $('.hamburger').on('click', function () {
+        $('.hamburger-menu').css('display', 'grid')   
+        })
+        let hamburgerMenu = Object.values(navbar.hamburgerMenu);
+        for (let value of hamburgerMenu) {
+            $('.menu-content').append(`
+            <div class="menu-content-text">
+                <a href="${value.href}">${value.text}</a>
+            </div>
+        `)
+        }
+      },
+      closeMenu: () => {
+          $(document).on('keydown', function(e) {
+            if (e.keyCode == 27) {
+                $('.hamburger-menu').css('display', 'none')
+            }
+          })
+          $('.escape').on('click', function() {
+            $('.hamburger-menu').css('display', 'none')
+          })
       }
     };
 
@@ -49,6 +90,10 @@ function createNavBar() {
 
     // Display social links
     navbar.showLinks()
+
+    // Hamburger Menu
+    navbar.showMenu()
+    navbar.closeMenu()
     
 };
 
