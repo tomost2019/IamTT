9a79ff31a26dba6387da1040fddd1116c0205a64
Tom Öst <tom.ost@yahoo.com>
2020-02-09 04:30:27 +0700

    Completed about section, contact, copyright and social icons. Added drift insta messages

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,7 @@ const navbar = {
         },
         link3: {
             text: "Contact",
-            href: ""
+            href: "#contact-sitemap"
         },
       },
       showText: () => {
@@ -52,6 +52,11 @@ const navbar = {
                 <div class="underscore"></div>
                 <span class="open">Open</span>
             </div>
+        `);
+        $(".social-icons-bottom").append(`
+            <a href="${value.link}" target="_rel">
+            <img src="${value.imgSrc}" alt="${value.name}">
+            </a>
         `);
         }
       },
@@ -279,6 +284,34 @@ function showProjects() {
     projects.cardShowSkills();
 }
 
+// Drift (chat) code
+function drift() {
+    "use strict";
+    !function() {
+        var t = window.driftt = window.drift = window.driftt || [];
+        if (!t.init) {
+          if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
+          t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
+          t.factory = function(e) {
+            return function() {
+              var n = Array.prototype.slice.call(arguments);
+              return n.unshift(e), t.push(n), t;
+            };
+          }, t.methods.forEach(function(e) {
+            t[e] = t.factory(e);
+          }), t.load = function(t) {
+            var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+            o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+            var i = document.getElementsByTagName("script")[0];
+            i.parentNode.insertBefore(o, i);
+          };
+        }
+      }();
+      drift.SNIPPET_VERSION = '0.3.1';
+      drift.load('kvx7dnbxr8t9');
+}
+
 // Show Portfolio
 createNavBar()
 showProjects()
+drift()
